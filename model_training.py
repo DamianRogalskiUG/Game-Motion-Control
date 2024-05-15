ba50b06abd0202d94d600beb0b22ff8f265e09c2
Damian <d.rogalski.417@studms.ug.edu.pl>
2024-05-15 23:06:03 +0200

    Refactoring model_training.py

--- model_training.py
+++ model_training.py
@@ -1,10 +1,11 @@
 import numpy as np
+import math
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 
 
-# data dirs
+# data directories
 train_data_dir = 'Data/Train'
 test_data_dir = 'Data/Test'
 
@@ -14,17 +15,21 @@ batch_size = 32
 
 # generator for training dataset
 train_datagen = ImageDataGenerator(rescale=1./255)
-train_generator = train_datagen.flow_from_directory(train_data_dir,
-                                                    target_size=(image_size, image_size),
-                                                    batch_size=batch_size,
-                                                    class_mode='categorical')
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(image_size, image_size),
+    batch_size=batch_size,
+    class_mode='categorical'
+)
 
 # generator for testing dataset
 test_datagen = ImageDataGenerator(rescale=1./255)
-test_generator = test_datagen.flow_from_directory(test_data_dir,
-                                                  target_size=(image_size, image_size),
-                                                  batch_size=batch_size,
-                                                  class_mode='categorical')
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(image_size, image_size),
+    batch_size=batch_size,
+    class_mode='categorical'
+)
 
 # model definition
 model = Sequential()
@@ -39,19 +44,18 @@ model.add(Dense(128, activation='relu'))
 model.add(Dense(4, activation='softmax'))
 
 # model compile
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # model training
-history = model.fit(train_generator,
-                    steps_per_epoch=train_generator.samples // batch_size,
-                    epochs=10)
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // batch_size,
+    epochs=10
+)
 
 # model evaluation
 loss, accuracy = model.evaluate(test_generator)
-print("Test Accuracy: {:.2f}%".format(accuracy * 100))
-
+print(f"Model Accuracy: {math.floor(accuracy * 100) / 100}")
 
 # save the model
 model.save('Model/hand_gesture_model.keras')
